fda6a3a7596b56d59b6fc638895b4856d21b6063
lancelotly <lancelotly@users.noreply.github.com>
2019-06-20 11:15:46 +0800

    Update hypothes.isFloatingButton.js

--- hypothes.isFloatingButton.js
+++ hypothes.isFloatingButton.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            hypothes.isFloatingButton
 // @namespace       https://greasyfork.org/users/296362
-// @version         1.0.2.3
+// @version         1.0.2.4
 // @author          Lancelotly.Sagirrarimeow
 // @description     This is a shortcut for using hypothes.is service. It combines the hypothes.is bookmarklet to the page directly for a further convience.
 // @match           *://*/*
@@ -13,13 +13,13 @@
 // ==/UserScript==
 
 /*--- Create a button in a container div.  It will be styled and positioned with CSS.*/
-var zNode=document.createElement("div");function buttonClickAction(t){!function(){window.hypothesisConfig=function(){return{showHighlights:!0,appType:"bookmarklet"}};var t=document,e=t.createElement("script");e.setAttribute("src","https://hypothes.is/embed.js"),t.body.appendChild(e)}()}zNode.innerHTML='<div id="fButton" data-toggle="tooltip" data-placement="left" data-original-title="Create"></div>',zNode.setAttribute("id","myFloatingButton"),document.body.appendChild(zNode),document.getElementById("fButton").addEventListener("click",buttonClickAction,!1);
+function buttonClickAction(t){!function(){window.hypothesisConfig=function(){return{showHighlights:!0}};var t=document,e=t.createElement("script");e.setAttribute("src","https://hypothes.is/embed.js"),t.body.appendChild(e)}()}function findHighestZIndex(t){for(var e=document.getElementsByTagName(t),n=0,d=0;d<e.length;d++){var i=document.defaultView.getComputedStyle(e[d],null).getPropertyValue("z-index");i>n&&"auto"!=i&&(n=i)}return n}var hNode=document.createElement("div"),z=findHighestZIndex("div");hNode.innerHTML='<div id="fButton" data-toggle="tooltip" data-placement="left" data-original-title="Create"></div>',hNode.style.zIndex=z+1,hNode.setAttribute("id","myFloatingButton"),document.body.appendChild(hNode),document.getElementById("fButton").addEventListener("click",buttonClickAction,!1);
 
 //--- Style our newly added elements using CSS.
 GM_addStyle(`
 #myFloatingButton {
-    width: 2em;
-    height: 2em;
+    width: 25px;
+    height: 25px;
     border-radius: 50%;
     background-image: url(https://blogresources.lancelotly.ml/img/hypothesis-favicon.png);
     background-repeat: no-repeat;
@@ -27,7 +27,7 @@ GM_addStyle(`
     position: fixed;
     bottom: 20px;
     left: 20px;
-    box-shadow: 0px 2px 6px #666;
+    box-shadow: -2px 2px 9px #202020;
 }
 
 #fButton {
